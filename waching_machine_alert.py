e0c1086454789e09579c7152633fd6a8c7e3721c
Fabian Wörenkämper <f.woerenkaemper@krombacher.de>
2024-03-21 16:04:41 +0100

    wachhing-machine-alert

--- waching_machine_alert.py
+++ waching_machine_alert.py
@@ -32,16 +32,17 @@ def send_pushover_notification(message):
 async def monitor_power_and_notify(device, threshold_high=50, threshold_low=10, duration_minutes=5):
     power_exceeded = False
     low_power_start_time = None
+    sensor_name = 'current_power'
 
     while True:
         current_power = (await device.get_current_power()).to_dict()
-        print(f"Current power: {current_power['current_power']}W")  # For debugging
+        print(f"Current power: {current_power[sensor_name]}W")  # For debugging
 
-        if current_power['power'] > threshold_high:
+        if current_power[sensor_name] > threshold_high:
             power_exceeded = True
             low_power_start_time = None  # Reset since power is high again
 
-        if power_exceeded and current_power['power'] < threshold_low:
+        if power_exceeded and current_power[sensor_name] < threshold_low:
             if low_power_start_time is None:
                 low_power_start_time = datetime.now()
             elif datetime.now() - low_power_start_time > timedelta(minutes=duration_minutes):
