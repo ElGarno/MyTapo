21ec52e24bd65b3c7dc4e491c51ec666113e9363
Fabian Wörenkämper <f.woerenkaemper@krombacher.de>
2024-03-25 16:32:20 +0100

    also push messages for solar panel

--- solar_energy_generated.py
+++ solar_energy_generated.py
@@ -7,16 +7,18 @@ from utils import get_energy_data_daily, get_date_df_from_dict, send_pushover_no
 
 
 async def monitor_generated_solar_energy_and_notify(device_solar, user):
-    dict_energy_data_daily = (await get_energy_data_daily(device_solar)).to_dict()
-    df_energy_consumption = get_date_df_from_dict(dict_energy_data_daily)
-    df_energy_consumption.set_index('Date', inplace=True)
-    solar_energy_generated_today = df_energy_consumption.loc[str(datetime.today().date())]['Value']
-    max_solar_energy = df_energy_consumption['Value'].max()
-    message = f"The energy consumed today has been {solar_energy_generated_today / 1000:.4g} kWh which is {solar_energy_generated_today / max_solar_energy:.1%} of the maximum energy generated this year."
-    # send notification every day at 11pm
-    print(message)
-    if (datetime.now().hour == 23) and (datetime.now().minute == 0):
-        send_pushover_notification(user, message)
+    while True:
+        dict_energy_data_daily = (await get_energy_data_daily(device_solar)).to_dict()
+        df_energy_consumption = get_date_df_from_dict(dict_energy_data_daily)
+        df_energy_consumption.set_index('Date', inplace=True)
+        solar_energy_generated_today = df_energy_consumption.loc[str(datetime.today().date())]['Value']
+        max_solar_energy = df_energy_consumption['Value'].max()
+        message = f"The energy consumed today has been {solar_energy_generated_today / 1000:.4g} kWh which is {solar_energy_generated_today / max_solar_energy:.1%} of the maximum energy generated this year."
+        # send notification every day at 11pm
+        print(message)
+        if (datetime.now().hour == 23) and (datetime.now().minute == 0):
+            send_pushover_notification(user, message)
+        await asyncio.sleep(60)
 
 
 
